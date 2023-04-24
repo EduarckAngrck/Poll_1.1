67408a8ff66ef8cb6dc795a9c75845c5c2a244ee
Jojos <ag432831@uaeh.edu.mx>
2023-04-24 00:06:24 -0600

    Intendo hacer el contenido adaptable

--- src/Vistas/ajustes.java
+++ src/Vistas/ajustes.java
@@ -8,6 +8,11 @@ import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 /**
@@ -22,14 +27,8 @@ public class ajustes extends javax.swing.JFrame {
     public ajustes() {
         initComponents();
         this.setLocationRelativeTo(null);
-        // Obtener el tamaño de la pantalla
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = screenSize.width;
-        int height = screenSize.height;
         
-        int windowWidth = (int) (width * 0.8);
-        int windowHeight = (int) (height * 0.8);
-        setSize(windowWidth, windowHeight);
+        // Obtener el tamaño de la pantalla
     }
 
     /**
@@ -42,7 +41,7 @@ public class ajustes extends javax.swing.JFrame {
     private void initComponents() {
 
         Regresar = new javax.swing.JButton();
-        jLabel_fond = new javax.swing.JLabel();
+        Fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -55,13 +54,10 @@ public class ajustes extends javax.swing.JFrame {
         });
         getContentPane().add(Regresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 70, 100, 50));
 
-        jLabel_fond.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Mesa_principal.png"))); // NOI18N
-        jLabel_fond.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                jLabel_fondKeyPressed(evt);
-            }
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Mesa_principal.png"))); // NOI18N
+        Fondo.addKeyListener(new java.awt.event.KeyAdapter() {
         });
-        getContentPane().add(jLabel_fond, new org.netbeans.lib.awtextra.AbsoluteConstraints(-40, -10, 1080, 600));
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(-40, -10, 1080, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -75,9 +71,6 @@ public class ajustes extends javax.swing.JFrame {
         
     }//GEN-LAST:event_RegresarActionPerformed
 
-    private void jLabel_fondKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jLabel_fondKeyPressed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jLabel_fondKeyPressed
 
     /**
      * @param args the command line arguments
@@ -115,10 +108,76 @@ public class ajustes extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Fondo;
     private javax.swing.JButton Regresar;
-    private javax.swing.JLabel jLabel_fond;
     // End of variables declaration//GEN-END:variables
 
 
 
 }
+
+/*  import javax.swing.JFrame;
+    import javax.swing.JButton;
+    import javax.swing.GroupLayout;
+    import javax.swing.LayoutStyle.ComponentPlacement;
+    import java.awt.Container;
+    import java.awt.Dimension;
+    import java.awt.Toolkit;
+
+    public class VentanaAdaptable extends JFrame {
+    
+    public VentanaAdaptable() {
+        super("Ventana Adaptable");
+        
+        // Crear los componentes de la ventana
+        JButton boton1 = new JButton("Botón 1");
+        JButton boton2 = new JButton("Botón 2");
+        JButton boton3 = new JButton("Botón 3");
+        
+        // Crear el contenedor principal y asignarle el layout GroupLayout
+        Container contenedor = getContentPane();
+        GroupLayout layout = new GroupLayout(contenedor);
+        contenedor.setLayout(layout);
+        
+        // Configurar el layout para que los componentes se ajusten automáticamente
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        
+        // Definir las restricciones de los componentes en el layout
+        layout.setHorizontalGroup(
+            layout.createSequentialGroup()
+                .addComponent(boton1)
+                .addComponent(boton2)
+                .addComponent(boton3)
+        );
+        
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(boton1)
+                    .addComponent(boton2)
+                    .addComponent(boton3))
+        );
+        
+        // Obtener el tamaño de la pantalla
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+        
+        // Definir el tamaño de la ventana como un porcentaje de la pantalla
+        int windowWidth = (int) (width * 0.8);
+        int windowHeight = (int) (height * 0.8);
+        setSize(windowWidth, windowHeight);
+        
+        // Centrar la ventana en la pantalla
+        setLocationRelativeTo(null);
+        
+        // Hacer visible la ventana
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new VentanaAdaptable();
+    }
+}
+*/
